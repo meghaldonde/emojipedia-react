97253896cd814abc102ff7b418b69c0921fff7de
meghaldonde <meghal.donde@gmail.com>
2020-08-17 17:37:04 -0400

    change package.json to deploy github pages

---
+++ build/precache-manifest.92b0fb834869ff8081304565763087db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5014cda58f86238fe5c581eece884bce",
+    "url": "/emojipedia-react/index.html"
+  },
+  {
+    "revision": "c082d04888135ca031d1",
+    "url": "/emojipedia-react/static/js/2.fb29f6a2.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/emojipedia-react/static/js/2.fb29f6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1939d52b8583be5f5d66",
+    "url": "/emojipedia-react/static/js/main.8c3a8a85.chunk.js"
+  },
+  {
+    "revision": "cff94afb5184a437720a",
+    "url": "/emojipedia-react/static/js/runtime-main.e1ec4c12.js"
+  }
+]);
